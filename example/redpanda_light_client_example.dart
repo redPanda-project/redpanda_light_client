7b2f8bb6ed05b32faa4ca088b802dbd5363b172d
Robin Braun <RobinFlohr@gmail.com>
2020-03-08 16:37:05 +0100

    init commit of dart lib example + gitignore

---
+++ example/redpanda_light_client_example.dart
@@ -0,0 +1,6 @@
+import 'package:redpanda_light_client/redpanda_light_client.dart';
+
+main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/redpanda_light_client_test.dart
@@ -0,0 +1,16 @@
+import 'package:redpanda_light_client/redpanda_light_client.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    Awesome awesome;
+
+    setUp(() {
+      awesome = Awesome();
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
