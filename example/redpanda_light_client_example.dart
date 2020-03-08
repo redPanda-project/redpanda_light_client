d1935343d9b7179b9f55e18a498e7c2c6e817853
Robin Braun <RobinFlohr@gmail.com>
2020-03-08 17:19:33 +0100

    first code copied from flutter app

--- example/redpanda_light_client_example.dart
+++ example/redpanda_light_client_example.dart
@@ -1,6 +1,9 @@
 import 'package:redpanda_light_client/redpanda_light_client.dart';
+import 'package:redpanda_light_client/src/main/KademliaId.dart';
 
 main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+  var awesome = RedPandaLightClient();
+  print('awesome: ${RedPandaLightClient.test}');
+
+  RedPandaLightClient.init(new KademliaId());
 }
--- lib/src/redpanda_light_client_base.dart
+++ lib/src/redpanda_light_client_base.dart
@@ -1,6 +1,26 @@
 // TODO: Put public facing types in this file.
 
+import 'package:redpanda_light_client/src/main/Channel.dart';
+import 'package:redpanda_light_client/src/main/ConnectionService.dart';
+import 'package:redpanda_light_client/src/main/KademliaId.dart';
+
 /// Checks if you are awesome. Spoiler: you are.
-class Awesome {
-  bool get isAwesome => true;
+class RedPandaLightClient {
+  static List<Channel> _channels;
+  static String test = "test";
+
+  static init(KademliaId myKademliaId) {
+    ConnectionService connectionService = ConnectionService(myKademliaId);
+    connectionService.start();
+  }
+
+  static List<Channel> getChannels() {
+    if (_channels == null) {
+      _channels = new List<Channel>();
+      _channels.add(new Channel("Name 1"));
+      _channels.add(new Channel("Name 2"));
+    }
+
+    return _channels;
+  }
 }
